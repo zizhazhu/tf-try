4dc69caed35430273fdff86a82b074225eb9c1f4
zizhazhu <zizhazhu@gmail.com>
2018-08-10 15:38:16 +0800

    add sequence length

--- rnn/dynamic_rnn_eager.py
+++ rnn/dynamic_rnn_eager.py
@@ -13,10 +13,12 @@ X = np.array(
         [[6, 7, 8], [6, 5, 4]],
         [[9, 0, 1], [3, 2, 1]],
     ], dtype=np.float32)
+X_length = np.array([2, 1, 2, 2])
 
 basic_cell = tf.nn.rnn_cell.BasicRNNCell(num_units=n_neurons)
 output_seqs, states = tf.nn.dynamic_rnn(
-    basic_cell, X, dtype=tf.float32
+    basic_cell, X, dtype=tf.float32, sequence_length=X_length
 )
 
 print(output_seqs)
+print(states)
