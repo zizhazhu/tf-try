baabfa45a8c7464f045e94b618ed4756fb1e8cde
zizhazhu <zizhazhu@gmail.com>
2018-09-21 16:46:50 +0800

    update features

--- autoencoder/stacked.py
+++ autoencoder/stacked.py
@@ -1,8 +1,13 @@
+import argparse
 import tensorflow as tf
 import tensorflow.contrib as tfc
 import matplotlib.pyplot as plt
 
 import data.mnist as mnist
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--show', action='store_true')
+args = parser.parse_args()
 
 tf.enable_eager_execution()
 
@@ -33,28 +38,30 @@ class Stacked(tf.keras.Model):
         outputs = self.outputs(layer)
         return outputs
 
-    def loss(self, X):
-        y = self.call(X)
+    def loss(self, X, y):
         reconstruction_loss = tf.reduce_mean(tf.square(X - y))
         reg_loss = tfc.layers.apply_regularization(tfc.layers.l2_regularizer(l2_reg), self.variables)
         loss = tf.reduce_sum(reg_loss) + reconstruction_loss
-        return loss, tf.reshape(y, [-1, 28, 28])
+        return loss
 
 
 optimizer = tf.train.AdamOptimizer(learning_rate)
 model = Stacked(n_hidden)
-train_dataset = mnist.train('temp/mnist').batch(64).repeat(10)
+train_dataset = mnist.train('temp/mnist').batch(64).repeat(1)
 plt.axis('off')
 for index, (images, _) in enumerate(train_dataset):
     with tf.GradientTape() as tape:
-        loss, y = model.loss(images)
+        y = model(images)
+        loss = model.loss(images, y)
     grads = tape.gradient(loss, model.variables)
     optimizer.apply_gradients(zip(grads, model.variables))
     if index % 100 == 0:
-        plt.suptitle(index)
-        plt.subplot(121)
-        plt.imshow(tf.reshape(images[0], [28, 28]))
-        plt.subplot(122)
-        plt.imshow(y[0])
-        plt.show()
+        print('step{}: {}'.format(index, loss))
+        if args.show:
+            plt.suptitle(index)
+            plt.subplot(121)
+            plt.imshow(tf.reshape(images[0], [28, 28]))
+            plt.subplot(122)
+            plt.imshow(tf.reshape(y[0], [28, 28]))
+            plt.show()
 
